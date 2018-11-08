b08abe0de11259b933db389c0f4073e94726c373
Alice Butcher <alycejenni@gmail.com>
2018-11-08 09:54:31 +0000

    :recycle: moved filetypes into a module

--- autocorrect/models/filetypes/_py.py
+++ autocorrect/models/filetypes/_py.py
@@ -1,35 +1,10 @@
 import redbaron
 
-from autocorrect.helpers.bools import isdocumentable, isforbidden, isroot
+from autocorrect.helpers.bools import isdocumentable, isroot, isforbidden
 from autocorrect.helpers.constants import typenames
 from autocorrect.helpers.correctors import PyCorrector
-from autocorrect.helpers.methods import get_dotproxy_names, nodes_stats, pretty
-
-
-class FileItem(object):
-    '''
-    A generic file type.
-    '''
-    ext_ = None
-
-    def __init__(self, meta):
-        self.meta = meta
-
-    def read(self):
-        with open(self.meta.path, 'r') as f:
-            return f.read()
-
-    def correct(self, *methods):
-        raise NotImplementedError('corrections not implemented for this filetype')
-
-    @property
-    def stats(self):
-        return {}
-
-    def __repr__(self):
-        content = [item for sublist in [pretty(k, v, 0) for k, v in self.stats.items()]
-                   for item in sublist]
-        return '\n'.join([self.meta.path] + content) + '\n'
+from autocorrect.helpers.methods import get_dotproxy_names, nodes_stats
+from autocorrect.models.filetypes._base import FileItem
 
 
 class Pyfile(FileItem):
--- setup.py
+++ setup.py
@@ -1,17 +1,41 @@
-from setuptools import setup, find_packages
+# !/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import find_packages, setup
+
+NAME = 'autocorrect'
+DESCRIPTION = 'A CLI-based tool for making bulk changes to file text.'
+URL = 'https://github.com/alycejenni/autocorrect'
+EMAIL = 'alycejenni@gmail.com'
+AUTHOR = 'Alice Butcher'
+VERSION = '0.1.3'
+
+REQUIRED = ['click', 'redbaron']
 
 setup(
-    name='autocorrect',
-    version='0.1.2',
-    packages=find_packages(),
-    package_data={'autocorrect': ['data/autocorrect.json']},
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    package_data={
+        'autocorrect': ['data/autocorrect.json']
+        },
     include_package_data=True,
-    install_requires=[
-        'click',
-        'redbaron'
-        ],
     entry_points='''
         [console_scripts]
         autocorrect=autocorrect.cli:cli
     ''',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython'
+        ]
     )
